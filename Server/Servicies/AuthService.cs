d910fe1b7cc4849ac7bc346e5672115c13ef92fb
Rreems <walen212@gmail.com>
2025-04-08 15:23:57 +0200

    Création fichiers Service

---
+++ Server/Servicies/AuthService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarMember_server.Servicies
+{
+    public class AuthService : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Server/Servicies/ReviewService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarMember_server.Servicies
+{
+    public class ReviewService : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
