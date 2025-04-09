cb97f97bb1a73a204e93a438d04920285bb1d1a5
Cyril <cyril.leducq.pro@gmail.com>
2025-04-09 16:30:14 +0200

    Creation du dossier RidesDTO et initialisation de tous les DTOsRide

---
+++ Server/DTOs/RidesDTO/AddPassengerRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarMember_server.DTOs.RidesDTO
+{
+    public class AddPassengerRequestDTO
+    {
+        [Required(ErrorMessage = "L'ID du trajet est obligatoire.")]
+        public Guid RideId { get; set; }
+
+        [Required(ErrorMessage = "L'ID de l'utilisateur (passager) est obligatoire.")]
+        public Guid UserId { get; set; }
+    }
+}
