9daef25a478845d35f3a1ebdb25fbda03d511295
Cyril <cyril.leducq.pro@gmail.com>
2025-04-10 10:09:48 +0200

    Ajout des DTOs UserAverageView et UserViewAllReviews

---
+++ Server/DTOs/UsersDTO/UserAverageViewRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace CarMember_server.DTOs.UsersDTO
+{
+    public class UserAverageViewRequestDTO
+    {
+        public Guid UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public double AverageScore { get; set; }
+
+        public int ReviewCount { get; set; }
+    }
+}
---
+++ Server/DTOs/UsersDTO/UserViewAllReviewsResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace CarMember_server.DTOs.UsersDTO
+{
+    public class UserViewAllReviewsResponseDTO
+    {
+        public List<ReviewResponseDTO> Reviews { get; set; }
+    }
+
+    public class ReviewResponseDTO
+    {
+        public Guid Id { get; set; }
+        public int Score { get; set; }
+        public string Comment { get; set; }
+        public string AuthorUserName { get; set; } 
+    }
+}
