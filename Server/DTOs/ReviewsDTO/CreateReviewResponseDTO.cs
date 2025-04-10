cba494d0291d22f77d43f66178c2b70e7c7a6841
Cyril <cyril.leducq.pro@gmail.com>
2025-04-10 11:30:01 +0200

    Correction de la loginResponseDTO

--- Server/DTOs/ReviewsDTO/CreateReviewResponseDTO.cs
+++ Server/DTOs/ReviewsDTO/CreateReviewResponseDTO.cs
@@ -4,7 +4,7 @@
     {
         public bool IsSuccessful { get; set; }
 
-        public string ErrorMessage { get; set; }
+        public string? ErrorMessage { get; set; }
 
         public Guid Id { get; set; }
 
