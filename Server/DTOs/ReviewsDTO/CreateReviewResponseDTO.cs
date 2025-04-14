c3f86aef0a4ccdf16659b8ad8dc088b742b0a8c9
Cyril <cyril.leducq.pro@gmail.com>
2025-04-14 11:01:55 +0200

    Rajout de la response DTO de reviews create

--- Server/DTOs/ReviewsDTO/CreateReviewResponseDTO.cs
+++ Server/DTOs/ReviewsDTO/CreateReviewResponseDTO.cs
@@ -2,22 +2,10 @@
 {
     public class CreateReviewResponseDTO
     {
-        public bool IsSuccessful { get; set; }
-
-        public string? ErrorMessage { get; set; }
-
         public Guid Id { get; set; }
-
         public int Score { get; set; }
-
         public string Comment { get; set; }
-
         public Guid AuthorUserId { get; set; }
-        public string? AuthorUserName { get; set; }
-
         public Guid ReviewedUserId { get; set; }
-
-        public string? ReviewedUserName { get; set; }
-        //
     }
 }
