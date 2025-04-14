add0c18496b66747b6dbec2c14b23db04b646c52
Cyril <cyril.leducq.pro@gmail.com>
2025-04-14 10:02:05 +0200

    correction RideUser avec lid en required

--- Server/Models/RideUser.cs
+++ Server/Models/RideUser.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CarMember_server.Models;
 
@@ -7,13 +8,14 @@ public class RideUser
     public Guid Id { get; set; }
 
 
-
+    [Required]
     [Column("user_id")]
     public Guid UserId { get; set; }
     public User User { get; set; }
 
-
+    [Required]
     [Column("ride_id")]
     public Guid RideId { get; set; }
     public Ride Ride { get; set; }
+
 }
