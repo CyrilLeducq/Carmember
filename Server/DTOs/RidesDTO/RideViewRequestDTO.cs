df800774519fc438d327885e1c495959ae1d9f32
Cyril <cyril.leducq.pro@gmail.com>
2025-04-14 11:14:33 +0200

    Ajout de RideViewRequetDTO et sa response

---
+++ Server/DTOs/RidesDTO/RideViewRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarMember_server.DTOs.RidesDTO
+{
+    public class RideViewRequestDTO
+    {
+        [Required]
+        public Guid RideId { get; set; }
+    }
+}
